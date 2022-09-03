3608e67806c9c1cb2cf033da6297b4e1e4960ad3
pauljnav <110696504+pauljnav@users.noreply.github.com>
2022-09-03 23:42:45 +0100

    Add files via upload

---
+++ 2022Feb_WarmUpExercises/PowerShellWarmUpExercise_Beginner.ps1
@@ -0,0 +1,13 @@
+# A PowerShell Warm-Up Exercise
+<#
+Beginner
+Write PowerShell code to take a string like ‘PowerShell’ and display it in reverse. Your solution can be a simple script or function.
+#>
+
+filter Invoke-ReverseString ([string]$str)
+{
+    $len = $str.Length
+    Write-Output ($str[-1..-$len] -join '')
+}
+
+Invoke-ReverseString "ABcd"
